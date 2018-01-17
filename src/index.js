0a8191bda4c48bffb4cf3db9dc08c70ed01265e5
misspia <pia_leung@hotmail.com>
2018-01-16 20:06:18 -0800

    set up webpack, react, project strcture

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return <div>
+      app root
+    </div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/themes/themes.js
@@ -0,0 +1,7 @@
+import Colors from './colors.js'
+import Fonts from './fonts.js'
+import Images from './images.js'
+import Metrics from './metrics.js'
+import Styles from './styles.js'
+
+export { Colors, Fonts, Images, Metrics, Styles };
